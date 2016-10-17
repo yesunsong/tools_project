39bb4d0a355aba4874823d00fca609cf3ba25ab2
yesunsong <1042282500@qq.com>
2016-10-17 12:28:15 +0800

    打出jar包后,读取与jar包同级的配置文件

--- GeneratePVR/src/AntSample.java
+++ GeneratePVR/src/AntSample.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileFilter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import net.sf.json.JSONArray;
@@ -11,6 +13,8 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.taskdefs.condition.Os;
 
+import cn.xm.yss.ArrayUtils;
+import cn.xm.yss.FileUtils;
 import cn.xm.yss.StringUtils;
 
 public class AntSample {
@@ -55,10 +59,31 @@ public class AntSample {
 	}
 
 	public static void main(String[] args) {
-		String JsonContext = new Util().ReadFile("./src/config.json");
+		String filePath = AntSample.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		// System.out.println("jar+path:" + filePath);
+
+		String user_dir = System.getProperty("user.dir");
+		// 配置文件的路径
+		String config_path = user_dir + File.separator + "generatePVR_cfg" + File.separator + "config.json";
+
+		System.out.println("-----config path:" + config_path);
+		// System.out.println(System.getProperty("user.home"));
+		// System.out.println("-----main_class_path:" +
+		// getClassFilePath(AntSample.class));
+		// System.out.println("-----" + getClassPath(AntSample.class));
+
+		// String xml_path =
+		// AntSample.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		String xml_path = user_dir + File.separator + "generatePVR_cfg" + File.separator + "generatePVR.xml";
+		System.out.println("-----xml path:" + xml_path);
+
+		String JsonContext = new Util().ReadFile(config_path);
+
 		JSONObject jsonObject = JSONObject.fromObject(JsonContext);
 		Resources_Path = (String) jsonObject.get("Resources_Path");
-		Resources_Path = Resources_Path.replace("/", "\\");
+		if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+			Resources_Path = Resources_Path.replace("/", "\\");	
+		}		
 		TexturePacker_Path = (String) jsonObject.get("TexturePacker_Path");
 
 		ExcludedDirs = JSONArray2JavaArray(jsonObject, "ExculdedDir", "dir");
@@ -79,20 +104,8 @@ public class AntSample {
 		System.out.println("Exculded File     :" + ExcludedFiles.toString());
 		System.out.println("Inculded Dir      :" + IncludedDirs.toString());
 		System.out.println("Inculded File	  :" + IncludedFiles.toString());
-		// }
-		File file = new File(Resources_Path);
-		File[] files = file.listFiles(new FileFilter() {
 
-			@Override
-			public boolean accept(File pathname) {
-				System.out.println(pathname.getPath());
-				String tmp = pathname.getPath().replace("\\", "/");
-				return tmp.equals(Resources_Path + "/button");
-			}
-		});
-		System.out.println("----------" + files.length);
-		//
-		File buildFile = new File(".//src//generatePVR.xml");
+		File buildFile = new File(xml_path);
 
 		// 创建一个ANT项目
 		p = new Project();
@@ -133,9 +146,10 @@ public class AntSample {
 		if (file.exists()) {
 			if (file.isDirectory()) {
 				checkDirectory(file);
-			} else {
-				checkFile(file);
 			}
+			// else {
+			// checkFile(file);
+			// }
 		}
 	}
 
@@ -149,14 +163,14 @@ public class AntSample {
 		}
 
 		// 需要检查该目录是否在排除目录里
-		boolean isIn = false;
-		for (String string : ExcludedDirs) {
-			if (string.equals(relativePath)) {
-				isIn = true;
-				ExcludedDirs.remove(string);
-				break;
-			}
-		}
+		boolean isIn = ArrayUtils.getInstance().removeWhenOccur(ExcludedDirs, relativePath);
+		// for (String string : ExcludedDirs) {
+		// if (string.equals(relativePath)) {
+		// isIn = true;
+		// ExcludedDirs.remove(string);
+		// break;
+		// }
+		// }
 
 		if (isIn) {
 			return;
@@ -170,25 +184,20 @@ public class AntSample {
 				// System.out.println(pathname.getPath());
 				String tmp = pathname.getName();
 
-				for (String string : ExcludedFiles) {
-					boolean isEqual = tmp.equals(string);
-					if (isEqual) {
-						ExcludedFiles.remove(string);
-					}
-					return !isEqual;
-				}
-				return true;
+				return !ArrayUtils.getInstance().removeWhenOccur(ExcludedFiles, tmp);
+				// for (String string : ExcludedFiles) {
+				// boolean isEqual = tmp.equals(string);
+				// if (isEqual) {
+				// ExcludedFiles.remove(string);
+				// }
+				// return !isEqual;
+				// }
+				// return true;
 			}
 		});
 
 		// 继续检查、打包目录、打包单文件
-		boolean hasDirectory = false;
-		for (int i = 0; i < files.length; i++) {
-			if (files[i].isDirectory()) {
-				hasDirectory = true;
-				break;
-			}
-		}
+		boolean hasDirectory = FileUtils.getInstance().hasDirectory(dir);
 
 		if (!hasDirectory) {// 不存在子目录，打包目录
 			if (files.length == dir.listFiles().length) {// 打包目录
@@ -202,49 +211,63 @@ public class AntSample {
 			if (file.isDirectory()) {// 继续检查目录
 				checkDirectory(file);
 			} else {// 打包目录或文件
-				if (!file.getName().endsWith(".png")) {
+				if (!checkFile(file)) {
 					continue;
 				}
-				single_target(p, file.getParentFile().getAbsolutePath(), file.getName().substring(0, file.getName().indexOf(".")));
+				// if (!file.getName().endsWith(".png")) {
+				// continue;
+				// }
+				// single_target(p, file.getParentFile().getAbsolutePath(),
+				// file.getName().substring(0, file.getName().indexOf(".")));
 			}
 		}
 	}
 
-	private static void checkFile(File file) {
+	private static boolean checkFile(File file) {
 		if (!file.getName().endsWith(".png")) {
-			return;
+			return false;
 		}
-		String relativePath = file.getPath().substring(file.getPath().indexOf(Resources_Path) + 1, file.getPath().length());
+		// String relativePath =
+		// file.getPath().substring(file.getPath().indexOf(Resources_Path) + 1,
+		// file.getPath().length());
 
-		for (String string : ExcludedFiles) {
-			if (relativePath.equals(string)) {
-				break;
-			}
-		}
+		// // TODO 未处理
+		// for (String string : ExcludedFiles) {
+		// if (relativePath.equals(string)) {
+		// // break;
+		// return;
+		// }
+		// }
 
 		// 文件是否在包含文件里
 		single_target(p, file.getParentFile().getAbsolutePath(), file.getName().substring(0, file.getName().indexOf(".")));
+		return true;
 	}
 
 	// 单个文件打包
 	private static void single_target(Project p, String folderPath, String foldername) {
 		System.out.println("single:" + folderPath + "\\" + foldername);
 
-		boolean isIn = false;
 		String relativePath = (foldername + ".png");
+		boolean isIn = ArrayUtils.getInstance().removeWhenOccur(IncludedFiles, relativePath);
 
-		for (String string : IncludedFiles) {
-			if (relativePath.equals(string)) {
-				isIn = true;
-				IncludedFiles.remove(string);
-				p.setUserProperty("PixelFormat", RGBA8888);
-				break;
-			}
-		}
-
-		if (!isIn) {
+		// for (String string : IncludedFiles) {
+		// if (relativePath.equals(string)) {
+		// isIn = true;
+		// IncludedFiles.remove(string);
+		// p.setUserProperty("PixelFormat", RGBA8888);
+		// break;
+		// }
+		// }
+		if (isIn) {
+			p.setUserProperty("PixelFormat", RGBA8888);
+		} else {
 			p.setUserProperty("PixelFormat", RGBA4444);
 		}
+
+		// if (!isIn) {
+		// p.setUserProperty("PixelFormat", RGBA4444);
+		// }
 
 		p.setUserProperty("folder_path", folderPath);
 		p.setUserProperty("filename", foldername);
@@ -256,22 +279,97 @@ public class AntSample {
 		System.out.println("directory:" + dirPath);
 		String relativePath = dirPath.substring(dirPath.indexOf(Resources_Path) + Resources_Path.length() + 1, dirPath.length());
 
-		boolean isIn = false;
-		for (String string : IncludedDirs) {
-			if (relativePath.equals(string)) {
-				isIn = true;
-				IncludedDirs.remove(string);
-				p.setUserProperty("PixelFormat", RGBA8888);
-				break;
-			}
-		}
+		boolean isIn = ArrayUtils.getInstance().removeWhenOccur(IncludedDirs, relativePath);
+		// for (String string : IncludedDirs) {
+		// if (relativePath.equals(string)) {
+		// isIn = true;
+		// IncludedDirs.remove(string);
+		// p.setUserProperty("PixelFormat", RGBA8888);
+		// break;
+		// }
+		// }
 
 		if (!isIn) {
 			p.setUserProperty("PixelFormat", RGBA4444);
+		} else {
+			p.setUserProperty("PixelFormat", RGBA8888);
 		}
 
 		// 目录是否是 包含目录，不是的话，是否有 包含文件
 		p.setUserProperty("directory_path", dirPath);
 		p.executeTarget(directory_target);
+	}
+
+	/**
+	 *
+	 * Description:同getClassFile 解决中文编码问题
+	 *
+	 * @param clazz
+	 * @return
+	 * @mail sunyujia@yahoo.cn
+	 * @since：Sep 21, 2008 1:10:12 PM
+	 */
+	public static String getClassFilePath(Class clazz) {
+		try {
+			return java.net.URLDecoder.decode(getClassFile(clazz).getAbsolutePath(), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	/**
+	 *
+	 * Description:取得当前类所在的文件
+	 *
+	 * @param clazz
+	 * @return
+	 * @mail sunyujia@yahoo.cn
+	 * @since：Sep 21, 2008 12:32:10 PM
+	 */
+	public static File getClassFile(Class clazz) {
+		URL path = clazz.getResource(clazz.getName().substring(clazz.getName().lastIndexOf(".") + 1) + ".class");
+		if (path == null) {
+			String name = clazz.getName().replaceAll("[.]", "/");
+			path = clazz.getResource("/" + name + ".class");
+		}
+		return new File(path.getFile());
+	}
+
+	/**
+	 *
+	 * Description: 同getClassPathFile 解决中文编码问题
+	 *
+	 * @param clazz
+	 * @return
+	 * @mail sunyujia@yahoo.cn
+	 * @since：Sep 21, 2008 1:10:37 PM
+	 */
+	public static String getClassPath(Class clazz) {
+		try {
+			return java.net.URLDecoder.decode(getClassPathFile(clazz).getAbsolutePath(), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	/**
+	 *
+	 * Description:取得当前类所在的ClassPath目录
+	 *
+	 * @param clazz
+	 * @return
+	 * @mail sunyujia@yahoo.cn
+	 * @since：Sep 21, 2008 12:32:27 PM
+	 */
+	public static File getClassPathFile(Class clazz) {
+		File file = getClassFile(clazz);
+		for (int i = 0, count = clazz.getName().split("[.]").length; i < count; i++)
+			file = file.getParentFile();
+		if (file.getName().toUpperCase().endsWith(".JAR!")) {
+			file = file.getParentFile();
+		}
+		return file;
 	}
 }
---
+++ GeneratePVR/src/MyTask.java
@@ -0,0 +1,56 @@
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+
+public class MyTask extends Task {
+	private String mDir;
+	private String mTarget;
+	// 使用ant获取文件列表
+	private Vector filesets = new Vector();
+
+	@Override
+	public void execute() throws BuildException {
+		System.out.println("super.execute");
+		System.out.println(mDir);
+		System.out.println(mTarget);
+
+		String[] includedFiles = null;
+		for (Iterator iterator = filesets.iterator(); iterator.hasNext();) {
+			FileSet fs = (FileSet) iterator.next();
+			DirectoryScanner ds = fs.getDirectoryScanner(getProject());
+			includedFiles = ds.getIncludedFiles();
+			System.out.println("++++++++++++");
+//			System.out.println(includedFiles.toString());
+
+			for (int i = 0; i < includedFiles.length; i++) {
+				String tmp = includedFiles[i];
+				if (!tmp.endsWith(".png")) {
+					continue;
+				}
+				getProject().setUserProperty("folder_path", fs.getDir().getPath());
+				getProject().setUserProperty("filename", tmp.substring(0, tmp.indexOf(".")));
+				getProject().executeTarget("single");
+			}
+		}
+
+	}
+
+	public void addFileset(FileSet fileset) {
+		filesets.add(fileset);
+	}
+
+	public void setTarget(String target) {
+		mTarget = target;
+	}
+
+	// 绝对路径
+	// 一定要是setXXX,XXX不区分大小
+	public void setDir(String dir) {
+		mDir = dir;
+	}
+
+}
